9ac829f04bd5ae85ed16d4240e25e8087b90c786
Tiago Henrique <thenriqueleite277@gmail.com>
2025-10-10 23:33:20 -0300

    implemente new dependencies

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import app from './app';
+import { prisma } from './utils/prisma';
+
+
+const PORT = 3000
+
+async function server(){
+
+    try{
+        // Testar conexao com o banco
+        await prisma.$connect()
+        console.log('Connected to database')
+
+        // Iniciar servidor
+        app.listen(PORT, () => {
+             console.log(`Server us running on port ${PORT}`)
+        })
+    }catch(error){
+        console.error('Failed to start server:', error)
+        process.exit(1)
+    }
+}
+
+server()
