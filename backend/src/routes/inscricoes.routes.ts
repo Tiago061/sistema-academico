32c44c2408ecca39dc8af99a14df7740cd46de70
Tiago Henrique <thenriqueleite277@gmail.com>
2025-10-12 02:00:59 -0300

    fix(routes): update Inscricao routes to use consistent request handling

--- backend/src/routes/inscricoes.routes.ts
+++ backend/src/routes/inscricoes.routes.ts
@@ -4,10 +4,10 @@ import { InscricaoController } from "../controllers/InscricaoController.controll
 const router = Router()
 const inscricoesController = new InscricaoController
 
-router.post('/', inscricoesController.create)
-router.get('/', inscricoesController.getAll)
-router.get('/:id', inscricoesController.getById)
-router.put('/:id', inscricoesController.update)
-router.get('/:id', inscricoesController.delete)
+router.get('/', (req, res, next) => inscricoesController.getAll(req, res, next));
+router.get('/:id', (req, res, next) => inscricoesController.getById(req, res, next));
+router.post('/', (req, res, next) => inscricoesController.create(req, res, next));
+router.put('/:id', (req, res, next) => inscricoesController.update(req, res, next));
+router.delete('/:id', (req, res, next) => inscricoesController.delete(req, res, next));
 
 export default router
