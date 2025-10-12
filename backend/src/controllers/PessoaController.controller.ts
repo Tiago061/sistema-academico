0b27c1baa5cd2b4e6b39645ce5688c14d1d73592
Tiago Henrique <thenriqueleite277@gmail.com>
2025-10-11 21:08:56 -0300

    feat(controller): implement PessoaController with CRUD operations for Pessoa

---
+++ backend/src/controllers/PessoaController.controller.ts
@@ -0,0 +1,70 @@
+import { createPessoaSchema, updatePessoaSchema } from "../schemas/pessoa.schema";
+import { PessoaService } from "../services/PessoaService.service";
+import { Request, Response, NextFunction } from "express";
+
+
+export class PessoaController {
+    private pessoaService: PessoaService;
+
+    constructor(){
+        this.pessoaService = new PessoaService()
+    }
+
+    // POST /inscricoes
+        async create(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try {
+                const data = createPessoaSchema.parse(req.body)
+                const novaPessoa = await this.pessoaService.createPessoa(data)
+    
+                return res.status(201).json(novaPessoa)
+            }catch(error){
+                next(error)
+            }
+        }
+    
+        // GET /inscricoes?ativo=true&cursoId=1
+        async getAll(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try{
+                const pessoas = await this.pessoaService.getPessoas()
+                return res.json(pessoas)
+    
+            }catch(error){
+                next(error);
+            }
+        }
+        
+        async getById(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try{
+                const id = parseInt(req.params.id)
+                const pessoas = await this.pessoaService.getPessoaById(id)
+                return res.json(pessoas)
+            } catch(error){
+                next(error)
+            }
+        }
+    
+        // PUT /inscricoes/:id
+        async update(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try{
+                const id = parseInt(req.params.id)
+                const data = updatePessoaSchema.parse(req.body)
+    
+                const pessoaAtualizada = await this.pessoaService.updatePessoa(id, data)
+    
+                return res.json(pessoaAtualizada)
+            }catch (error){
+                next(error)
+            }
+        }
+    
+        // DELETE /inscricoes/:id
+        async delete(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try{
+                const id = parseInt(req.params.id)
+                await this.pessoaService.deletePessoa(id)
+                return res.status(204).send()
+            }catch (error){
+                next(error)
+            }
+        }
+    }
