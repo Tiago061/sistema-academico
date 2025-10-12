72ebc2ad4c5b9184a73c3d1e48ccaa65706046be
Tiago Henrique <thenriqueleite277@gmail.com>
2025-10-12 02:00:51 -0300

    feat(controller): implement PessoaController with CRUD operations and ID validation

---
+++ backend/src/controllers/pessoa.controller.ts
@@ -0,0 +1,86 @@
+import { createPessoaSchema, updatePessoaSchema } from "../schemas/pessoa.schema";
+import { PessoaService } from "../services/PessoaService.service";
+import { Request, Response, NextFunction } from "express";
+
+
+export class PessoaController {
+    private pessoaService: PessoaService;
+    
+    constructor(){
+        this.pessoaService = new PessoaService()
+    }
+
+
+    // POST /pessoas
+        async create(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try {
+                const data = createPessoaSchema.parse(req.body)
+                const novaPessoa = await this.pessoaService.createPessoa(data)
+    
+                 console.log(novaPessoa)
+                return res.status(201).json(novaPessoa)
+            }catch(error){
+                next(error)
+            }
+        }
+    
+        // GET /pessoas?ativo=true&cursoId=1
+        async getAll(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+        
+            try{
+                const pessoas = await this.pessoaService.getPessoas()
+                console.log("GET /pessoas chamado!");
+                return res.json(pessoas)
+    
+            }catch(error){
+                next(error);
+            }
+        }
+        
+        async getById(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try{
+                const id = parseInt(req.params.id)
+                //retorna true se não for um número (ex: "abc", "", undefined, etc).
+                if(isNaN(id)){
+                    return res.status(400).json({error: "ID inválido"})
+                }
+                const pessoas = await this.pessoaService.getPessoaById(id)
+                return res.json(pessoas)
+            } catch(error){
+                next(error)
+            }
+        }
+    
+        // PUT /pessoas/:id
+        async update(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try{
+                const id = parseInt(req.params.id)
+                //retorna true se não for um número (ex: "abc", "", undefined, etc).
+                if(isNaN(id)){
+                    return res.status(400).json({error: "ID inválido"})
+                }
+                const data = updatePessoaSchema.parse(req.body)
+    
+                const pessoaAtualizada = await this.pessoaService.updatePessoa(id, data)
+    
+                return res.json(pessoaAtualizada)
+            }catch (error){
+                next(error)
+            }
+        }
+    
+        // DELETE /pessoas/:id
+        async delete(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
+            try{
+                const id = parseInt(req.params.id)
+                //retorna true se não for um número (ex: "abc", "", undefined, etc).
+                if(isNaN(id)){
+                    return res.status(400).json({error: "ID inválido"})
+                }
+                await this.pessoaService.deletePessoa(id)
+                return res.status(204).send()
+            }catch (error){
+                next(error)
+            }
+        }
+    }
